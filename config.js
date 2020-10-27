8cd6d0f84923dfc41c6efc53a97a8a238f8a5c76
TheRealVorteX <73510935+TheRealVorteX@users.noreply.github.com>
2020-10-27 00:21:58 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-    token: "NzQxNDM1OTEwODc2NTYxNDEw.Xy3iAA.3HltxZnAQSnjXNwZ3ZjRm1NN9nE",
-    prefix: "nx!",
-    vercion: "Beta v0.4 [Cambios a todos los comandos]"
-}
-
-module.exports = config;

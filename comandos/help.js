badd5b0b8f43ea7c3df76d3bea0215146c9c7b91
k6d <75655959+k6d@users.noreply.github.com>
2021-01-13 22:37:44 -0300

    Arreglar otra falta de ortografia.

--- comandos/help.js
+++ comandos/help.js
@@ -7,7 +7,7 @@ module.exports = (client, message, args) => {
         .setFooter("Pedido por " + message.author.username)
         .setTitle("Lista de comandos")
         .setThumbnail(client.user.avatarURL())
-        .addField(`**${client.config.vercion}**`, " ¡Hola! yo soy Nexus Canary, la vercion beta de Nexus, estoy aqui para ayudarte a pasar un buen rato en este servidor. A continuacion tienes todos los comandos que puedes utilizar conmigo.")
+        .addField(`**${client.config.version}**`, " ¡Hola! yo soy Nexus Canary, la vercion beta de Nexus, estoy aqui para ayudarte a pasar un buen rato en este servidor. A continuacion tienes todos los comandos que puedes utilizar conmigo.")
         .addField("> Links", `[Invitame a tu servidor!](https://discord.com/api/oauth2/authorize?client_id=741435910876561410&permissions=8&scope=bot) \n [Servidor de soporte](https://discord.gg/vGXgWz9)`)
         .addField("> ***:information_source: Informacion***", "`ping` `server` `user` `bot` `stats`")
         .addField("> ***:joy:  Diverción***", "`hola` `confession` `say` `8ball` `dado` `coin` `meme` `memei`")
